76d9280cd72fea3592d46f67c5b49b088ad66ffd
psebaraj <patrick.sebaraj@gmail.com>
2023-09-27 01:33:52 -0400

    fixing api endpoint

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -9,6 +9,12 @@ app.get("/health", (req, res) => {
   res.send("healthy");
 });
 
+
+app.get("/works", (req, res) => {
+  res.status(200);
+  res.send("API Works!");
+});
+
 app.listen(port, () => {
   return console.log(`Container listening on port ${port}`);
 });
