a853073443fc3560cf860d5a7c800cdd8b7ea770
psebaraj <patrick.sebaraj@gmail.com>
2023-09-26 23:11:31 -0400

    fixing ecs deployment

--- backend/lib/ecs-fargate-stack.ts
+++ backend/lib/ecs-fargate-stack.ts
@@ -1,8 +1,13 @@
 import * as cdk from "aws-cdk-lib";
-// import { Vpc } from "aws-cdk-lib/aws-ec2";
+import { Vpc } from "aws-cdk-lib/aws-ec2";
 import { Cluster, ContainerImage } from "aws-cdk-lib/aws-ecs";
-import { NetworkLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
+import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
 import { Construct } from "constructs";
+import { CfnOutput, CfnResource, Stack, StackProps } from "aws-cdk-lib";
+import { CfnIntegration, CfnRoute } from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpApi } from "@aws-cdk/aws-apigatewayv2-alpha";
+import path = require("path");
+
 
 export class EcsFargateStack extends cdk.Stack {
   constructor(
@@ -13,28 +18,64 @@ export class EcsFargateStack extends cdk.Stack {
   ) {
     super(scope, id, props);
 
-    // VPC
+    // // VPC
     // const vpc = new Vpc(this, "AdventuristaVPC", {
     //   maxAzs: 2,
     //   natGateways: 1,
     // });
 
-    // Fargate cluster
-    const cluster = new Cluster(this, "AdventuristaCluster", {
-      // vpc: vpc as any,
+    // // Fargate cluster
+    // const cluster = new Cluster(this, "AdventuristaCluster", {
+    //   vpc: vpc as any,
+    // });
+
+    // // Fargate service
+    // const backendService = new NetworkLoadBalancedFargateService(
+    //   this,
+    //   "backendService",
+    //   {
+    //     cluster: cluster,
+    //     memoryLimitMiB: 1024,
+    //     cpu: 512,
+    //     desiredCount: 2,
+    //     taskImageOptions: {
+    //       image: ContainerImage.fromAsset("../backend/server"),
+    //       environment: {
+    //         stageName: stageName,
+    //       },
+    //     },
+    //   }
+    // );
+
+    // // Health check
+    // backendService.targetGroup.configureHealthCheck({ path: "/health" });
+
+    // // Load balancer url
+    // new cdk.CfnOutput(this, "loadBalancerUrl", {
+    //   value: backendService.loadBalancer.loadBalancerDnsName,
+    //   exportName: "loadBalancerUrl",
+    // });
+
+    const vpc = new Vpc(this, "MyVpc", {
+      maxAzs: 3,
     });
 
-    // Fargate service
-    const backendService = new NetworkLoadBalancedFargateService (
+    const cluster = new Cluster(this, "MyCluster", {
+      vpc: vpc,
+    });
+
+    const fargate = new ApplicationLoadBalancedFargateService(
       this,
-      "backendService",
+      "MyFargateService",
       {
+        assignPublicIp: false,
         cluster: cluster,
-        memoryLimitMiB: 1024,
-		cpu: 512,
+        cpu: 512,
         desiredCount: 2,
+        memoryLimitMiB: 1024,
+        publicLoadBalancer: false,
         taskImageOptions: {
-          image: ContainerImage.fromAsset("../backend/server"),
+          image: ContainerImage.fromAsset(path.join(__dirname, "../server/")),
           environment: {
             stageName: stageName,
           },
@@ -42,13 +83,49 @@ export class EcsFargateStack extends cdk.Stack {
       }
     );
 
-    // Health check
-    backendService.targetGroup.configureHealthCheck({ path: "/health" });
+	fargate.targetGroup.configureHealthCheck({ path: "/health" });
 
-    // Load balancer url
-    new cdk.CfnOutput(this, "loadBalancerUrl", {
-      value: backendService.loadBalancer.loadBalancerDnsName,
-      exportName: "loadBalancerUrl",
+    const httpVpcLink = new CfnResource(this, "HttpVpcLink", {
+      type: "AWS::ApiGatewayV2::VpcLink",
+      properties: {
+        Name: "V2 VPC Link",
+        SubnetIds: vpc.privateSubnets.map((m) => m.subnetId),
+      },
     });
+
+    const api = new HttpApi(this, "HttpApiGateway", {
+      apiName: "ApigwFargate",
+      description:
+        "Integration between apigw and Application Load-Balanced Fargate Service",
+    });
+
+    const integration = new CfnIntegration(this, "HttpApiGatewayIntegration", {
+      apiId: api.httpApiId,
+      connectionId: httpVpcLink.ref,
+      connectionType: "VPC_LINK",
+      description: "API Integration with AWS Fargate Service",
+      integrationMethod: "ANY", // for GET and POST, use ANY
+      integrationType: "HTTP_PROXY",
+      integrationUri: fargate.listener.listenerArn,
+      payloadFormatVersion: "1.0", // supported values for Lambda proxy integrations are 1.0 and 2.0. For all other integrations, 1.0 is the only supported value
+    });
+
+    new CfnRoute(this, "Route", {
+      apiId: api.httpApiId,
+      routeKey: "ANY /{proxy+}", // for something more general use 'ANY /{proxy+}'
+      target: `integrations/${integration.ref}`,
+    });
+
+    new CfnOutput(this, "APIGatewayUrl", {
+      description: "API Gateway URL to access the GET endpoint",
+      value: api.url!,
+    });
+  }
+}
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
   }
 }
