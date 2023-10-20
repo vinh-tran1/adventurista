0d997404f9a246fb249373c743566385540cbb0c
psebaraj <patrick.sebaraj@gmail.com>
2023-10-20 13:05:58 -0400

    Adding events table to Dynamo

--- backend/lib/cdk-stack.ts
+++ backend/lib/cdk-stack.ts
@@ -14,7 +14,11 @@ import { HttpApi } from "@aws-cdk/aws-apigatewayv2-alpha";
 import path = require("path");
 import { Table, AttributeType } from "aws-cdk-lib/aws-dynamodb";
 import { PolicyStatement, Effect, AnyPrincipal } from "aws-cdk-lib/aws-iam";
-import { BlockPublicAccess, BucketEncryption, Bucket } from "aws-cdk-lib/aws-s3";
+import {
+  BlockPublicAccess,
+  BucketEncryption,
+  Bucket,
+} from "aws-cdk-lib/aws-s3";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -41,6 +45,15 @@ export class CdkStack extends Stack {
         type: AttributeType.STRING,
       },
       tableName: "users",
+      removalPolicy: RemovalPolicy.DESTROY, // change removal policy to RETAIL in prod
+    });
+
+    const eventsTable = new Table(this, "events", {
+      partitionKey: {
+        name: "eventId",
+        type: AttributeType.STRING,
+      },
+      tableName: "events",
       removalPolicy: RemovalPolicy.DESTROY, // change removal policy to RETAIL in prod
     });
 
@@ -79,6 +92,8 @@ export class CdkStack extends Stack {
           environment: {
             USERS_PRIMARY_KEY: "userId",
             USERS_TABLE_NAME: usersTable.tableName,
+            EVENTS_PRIMARY_KEY: "eventId",
+            EVENTS_TABLE_NAME: eventsTable.tableName,
           },
         },
       }
@@ -124,7 +139,7 @@ export class CdkStack extends Stack {
           "dynamodb:UpdateItem",
           "dynamodb:DeleteItem",
         ],
-        resources: [`${usersTable.tableArn}`],
+        resources: [`${usersTable.tableArn}`, `${eventsTable.tableArn}`],
         conditions: {
           ArnEquals: {
             "aws:PrincipalArn": `${fargate.taskDefinition.taskRole.roleArn}`,
@@ -136,6 +151,7 @@ export class CdkStack extends Stack {
     // R/W permissions for Fargate
     profPicBucket.grantReadWrite(fargate.taskDefinition.taskRole);
     usersTable.grantReadWriteData(fargate.taskDefinition.taskRole);
+	eventsTable.grantReadWriteData(fargate.taskDefinition.taskRole);
 
     // API Gateway
     const httpVpcLink = new CfnResource(this, "HttpVpcLink", {
