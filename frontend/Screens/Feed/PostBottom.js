76a95599217061b60e2ca54d2186f383ef99926f
Nikhil Ismail <nikhil.ismail20@gmail.com>
2023-11-24 19:06:42 -0500

    calendar page, add event from feed

--- frontend/Screens/Feed/PostBottom.js
+++ frontend/Screens/Feed/PostBottom.js
@@ -2,10 +2,40 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import BubbleText from "../../Shared/BubbleText";
+import axios from 'axios';
+
+// Redux
+import { useSelector } from 'react-redux';
+import { selectUserInfo } from '../../Redux/userSlice';
 
 const PostBottom = (props) => {
 
-    const { caption, tags } = props;
+    const { caption, tags, eventId } = props;
+
+    const user = useSelector(selectUserInfo);
+
+    const API_URL = process.env.REACT_APP_AWS_API_URL + 'events/going-to-event'
+
+    const handleAttendEvent = async () => {
+        try {
+            const response = await axios.post(API_URL, {
+            userId: user.userId,
+            eventId: eventId
+          });
+          if (response.status === 200) {
+            console.log("Successfully added event to your calendar");
+          } else {
+            console.log("Error adding this event to your calendar");
+          }
+        } catch (err) {
+          console.log(err);
+          console.log("An error occurred while adding this event to your calendar. Please try again.");
+        }
+    }
+
+    const handleSaveEvent = () => {
+
+    }
 
     return (
         <View style={styles.postBottom}>
@@ -18,10 +48,10 @@ const PostBottom = (props) => {
                     })}
                 </View>
                 <View style={{ flexDirection: "row", marginTop: 12.5 }}>
-                    <TouchableOpacity style={{ marginRight: 12.5 }}>
+                    <TouchableOpacity style={{ marginRight: 12.5 }} onPress={handleAttendEvent}>
                         <FontAwesomeIcon color={"#717171"} icon="calendar-plus" size={25} />
                     </TouchableOpacity>
-                    <TouchableOpacity style={{ marginTop: 1.5 }}>
+                    <TouchableOpacity style={{ marginTop: 1.5 }} onPress={handleSaveEvent}>
                         <FontAwesomeIcon  color={"#717171"} icon="bookmark" size={24} />
                     </TouchableOpacity>
                 </View>
