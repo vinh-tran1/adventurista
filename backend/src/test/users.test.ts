fcacb6f21ebed644dfcf19bf60c45f5ff8d8e2da
psebaraj <patrick.sebaraj@gmail.com>
2023-11-27 22:26:44 -0500

    more create user tests

--- backend/src/test/users.test.ts
+++ backend/src/test/users.test.ts
@@ -35,32 +35,46 @@ describe('Create User', () => {
 
         sandbox.stub(DocumentClient.prototype, 'put').returns(returnValueMock);
 
-        usersFunctions.hashPassword = jest.fn().mockResolvedValue(sampleUnhashedPassword);
+        // jest.mock('../src/users', () => ({
+        //   hashPassword: jest.fn(),
+        // }));
+        
+        // Set the mockResolvedValue for the hashPassword function
+        //(hashPassword as jest.Mock).mockResolvedValue(sampleUnhashedPassword);
 
         const result: User | string = await createUser(sampleEmail, sampleFirstName, sampleLastName, sampleUnhashedPassword);
         let user: User = sampleUser;
-        if (typeof result === 'string') {
-            expect(false).toEqual(true)
-        } else {
+        if (typeof result !== 'string') {
             user.userId = result.userId;
+            user.hashedPassword = result.hashedPassword;
         }
-        expect(user).toEqual(sampleUser);
+        expect(user).toEqual(result);
       });
 
-    // it('When email is already associated with another account', async () => {
-    //   const returnValueMock = {
-    //       promise () {
-    //         return {
-    //           Item: sampleUser,
-    //         };
-    //       },
-    //     } as unknown as Request <DocumentClient.GetItemOutput, AWSError>;
-              
-    //     const stub = sandbox.stub(DocumentClient.prototype, 'get').returns(returnValueMock);
-  
-    //     const result = await getUser(sampleUserId);
-    //     expect(result).toEqual(sampleUser);
-    // });
+    it('When email is already associated with another account', async () => {
+        const returnEmailExistsMock = {
+            promise () {
+              return {
+                Count: 1,
+              };
+            },
+          } as unknown as Request <DocumentClient.QueryOutput, AWSError>;
+                
+        sandbox.stub(DocumentClient.prototype, 'query').returns(returnEmailExistsMock);
+
+        const returnValueMock = {
+            promise() {
+                return {
+                    Item: sampleUser,
+                };
+            },
+        } as unknown as Request<DocumentClient.PutItemOutput, AWSError>;
+
+        sandbox.stub(DocumentClient.prototype, 'put').returns(returnValueMock);
+
+        const result: User | string = await createUser(sampleEmail, sampleFirstName, sampleLastName, sampleUnhashedPassword);
+        expect(result).toEqual("Email already in use");
+    });
   
     // it('When DynamoDB put fails', async () => {
     //   const returnValueMock = {
